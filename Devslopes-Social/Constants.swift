4d0d639f49e10909d1fb5943bb98dfc8a06cd90f
mshayanh13 <mohammadhemani@gmail.com>
2017-05-01 20:35:54 -0500

    Custom Views

---
+++ Devslopes-Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Devslopes-Social
+//
+//  Created by Mohammad Hemani on 5/1/17.
+//  Copyright © 2017 Mohammad Hemani. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0

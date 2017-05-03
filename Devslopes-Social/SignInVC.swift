1645268c00b93f4ad4aaae0267504248d682deae
mshayanh13 <mohammadhemani@gmail.com>
2017-05-02 20:19:42 -0500

    email auth

--- Devslopes-Social/SignInVC.swift
+++ Devslopes-Social/SignInVC.swift
@@ -13,9 +13,12 @@ import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     @IBAction func facebookLoginTapped(_ sender: UIButton) {
@@ -34,15 +37,67 @@ class SignInVC: UIViewController {
         }
         
     }
+    
+    @IBAction func emailPasswordLoginTapped(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if let error = error as NSError? {
+                    
+                    if let errorCode = FIRAuthErrorCode(rawValue: error.code) {
+                        
+                        switch (errorCode) {
+                        case .errorCodeUserNotFound:
+                            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                                if let error = error as NSError? {
+                                    
+                                    if let errorCode = FIRAuthErrorCode(rawValue: error.code) {
+                                        
+                                        switch (errorCode) {
+                                        case .errorCodeWeakPassword:
+                                            self.showErrorAlert(title: "Weak Password", message: "Please enter a stronger password")
+                                        default:
+                                            print(error.debugDescription)
+                                        }
+                                    }
+                                } else {
+                                    print("MSH: Successfully authenticated with Firebase with new email/password")
+                                }
+                            })
+                        case .errorCodeInvalidEmail:
+                            self.showErrorAlert(title: "Invalid Email Format", message: "Please enter a proper email address")
+                        case .errorCodeWeakPassword:
+                            self.showErrorAlert(title: "Weak Password", message: "Please enter a stronger password")
+                        case .errorCodeWrongPassword:
+                            self.showErrorAlert(title: "Wrong Password", message: "Please enter the correct password")
+                        default:
+                            print(error.debugDescription)
+                        }
+                    }
+                } else {
+                    print("MSH: Email user authenticated with Firebase")
+                }
+            })
+        }
+    }
 
     func firebaseAuth(_ credential: FIRAuthCredential) {
         FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
             if error != nil {
                 print("MSH: Unable to authenticate with Firebase: \(error.debugDescription)")
             } else {
-                print("MSH: Successfully authenticated with Firebase")
+                print("MSH: Facebook user authenticated with Firebase")
             }
         })
+    }
+    
+    func showErrorAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
     }
 
 }
